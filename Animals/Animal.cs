ebad065d0becfbab4547bb0668f5a979138c9bf7
hospogh <hospogh@gmail.com>
2017-04-10 23:00:22 -0700

    Add project files.

---
+++ Animals/Animal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows;
+using Windows;
+using System.IO;
+
+namespace Animals
+{
+    abstract class Animal
+    {
+        abstract public string Name { get; set; }
+        abstract public int Legs { get; }//abstract public/*private ?*/ int Legs { get; /*private ?*/set;}
+        public void Speak()
+        {
+            SoundPlayer player = new SoundPlayer(Path);
+            player.PlaySync();
+        }
+        protected string Path { get; set; }
+    }
+
+    class Cat : Animal
+    {
+        public Cat(String name)
+        {
+            Legs = 4;
+            Name = name;
+            Path = @"C:\Users\hovse_000\Documents\Visual Studio 2015\Projects\Animals\Animals\sounds\cat\cat.wav";
+        }
+        public override int Legs { get; }
+        public override string Name { get; set; }
+    }
+
+    class Dog : Animal
+    {
+        public Dog(String name)
+        {
+            Name = name;
+            Legs = 4;
+            Path = @"C:\Users\hovse_000\Documents\Visual Studio 2015\Projects\Animals\Animals\sounds\dog\Dog Growling And Barking-SoundBible.com-883632423.wav";
+        }
+        public override int Legs { get; }
+        public override string Name { get; set; }
+    }
+
+    class Bird : Animal
+    {
+        public Bird(String name)
+        {
+            Legs = 2;
+            Name = name;
+            Path = @"C:\Users\hovse_000\Documents\Visual Studio 2015\Projects\Animals\Animals\sounds\bird\killdeer_song-Mike_Koenig-1144525481.wav";
+        }
+        public override int Legs { get; }
+        public override string Name { get; set; }
+    }
+
+    class Lion : Animal
+    {
+        public Lion(String name)
+        {
+            Name = name;
+            Legs = 4;
+            Path = @"C:\Users\hovse_000\Documents\Visual Studio 2015\Projects\Animals\Animals\sounds\lion\Lion Roar-SoundBible.com-718441804.wav";
+        }
+        public override int Legs { get; }
+        public override string Name { get; set; }
+
+    }
+}
+//sounds from http://soundbible.com/
---
+++ Animals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal an1 = new Cat("Mrrrrrrro");
+            Console.WriteLine(an1.Name);
+            an1.Speak();
+
+            Animal an2 = new Dog("Chalo");
+            Console.WriteLine(an2.Name);
+            an2.Speak();
+
+            Animal an3 = new Bird("Kiwi");
+            Console.WriteLine(an3.Name);
+            an3.Speak();
+
+            Animal an4 = new Lion("Catrin");
+            Console.WriteLine(an4.Name);
+            an4.Speak();
+        }
+    }
+
+}
